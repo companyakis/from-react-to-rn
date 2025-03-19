43ae10947eea9f708e59742afd551c7075d1c58c
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-19 11:34:31 +0300

    Create part-b-0004-useEffect1-App.jsx

---
+++ part-b-0004-useEffect1-App.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+
+function App() {
+
+  const [age, setAge] = useState(35)
+
+  const [name, setName] = useState("Aygün")
+
+  function handleName() {
+    setName("Merve Aygün")
+  }
+
+  function handleAge() {
+    setAge(34)
+  }
+
+  useEffect(() => {
+    console.log("Always work!")
+  })
+
+  useEffect(() => {
+    console.log("First render!")
+  }, [])
+
+  useEffect(() => {
+    console.log("First render!- Name updated!")
+  }, [name])
+
+  useEffect(() => {
+    console.log("First render! - Age updated!")
+  }, [age])
+
+  return (
+    <>
+
+      <div>Name: {name} and age: {age}</div>
+
+      <div>
+        <button onClick={handleName}>Set Name</button>
+        <button onClick={handleAge}>Set Age</button>
+      </div>
+  
+    </>
+  );
+}
+
+export default App;
