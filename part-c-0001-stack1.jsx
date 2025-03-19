1ff0a104ae796b092c49f82e1d0481930acffcd8
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-19 09:01:08 +0300

    Create part-c-0001-stack1.jsx

---
+++ part-c-0001-stack1.jsx
@@ -0,0 +1,15 @@
+import { Stack } from "@mui/material";
+
+function App() {
+  return (
+
+    <Stack spacing={3}>
+      <div>Element 1</div>
+      <div>Element 2</div>
+      <div>Element 3</div>
+    </Stack>
+
+  );
+}
+
+export default App;
