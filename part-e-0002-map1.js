f04861dd103cd46bcf5670242d4f197e318a8fcc
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-20 21:30:34 +0300

    Create part-e-0002-map1.js

---
+++ part-e-0002-map1.js
@@ -0,0 +1,7 @@
+const salesByEmployees = [5000, 4565, 3280, 4700, 6250]
+
+const premiums = salesByEmployees.map(sales => sales * 0.15)
+
+console.log(premiums) // [ 750, 684.75, 492, 705, 937.5 ]
+
+
