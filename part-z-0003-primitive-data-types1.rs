2a3e6816c51f2e694f6c21a8e102392b90605dd7
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-24 22:00:32 +0300

    Create part-z-0003-primitive-data-types1.rs

---
+++ part-z-0003-primitive-data-types1.rs
@@ -0,0 +1,15 @@
+fn main() {
+
+    // i8 -> i128, positive, zero, negative
+
+    let diff: i8 = 10 - 21;
+
+    let x: i32 = 65477;
+
+    // u8 -> u128 positive or zero
+
+    let balance: u8 = 0;
+
+    let year: u16 = 2025;
+
+}
