5ecd57338beb32f362b7f979216ff2793230fbd6
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-24 21:01:58 +0300

    Create part-y-0008-function3.sol

---
+++ part-y-0008-function3.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+
+    function double(uint _number) public pure returns (uint) {
+
+        return _number * 2;
+    }
+
+}
