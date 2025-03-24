6ce626bb831d5818d730090b2f94c98ba1aba0b2
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-24 20:47:05 +0300

    Create part-y-0003-unsigned-integers.sol

---
+++ part-y-0003-unsigned-integers.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+
+    uint8 public a = 101;
+
+    uint16 public b = 5000;
+
+    uint256 public sum = a + b; 
+    
+}
