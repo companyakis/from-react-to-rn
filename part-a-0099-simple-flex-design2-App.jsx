fad73f3c0537ef6c3039aaaa72a93ca020653615
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-25 23:35:41 +0300

    Update part-a-0099-simple-flex-design2-App.jsx

--- part-a-0099-simple-flex-design2-App.jsx
+++ part-a-0099-simple-flex-design2-App.jsx
@@ -1,1 +1,22 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+
+    <div className='header'>Mustafa Buyukdereli</div>
+
+    <div className='container'>
+      <h1 className='title'>Welcome People</h1>
+      <h5 className='summary'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia ex, cumque vel quod dolorum quidem.</h5>
+      <p className='info'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis voluptates saepe iusto, nostrum iste ullam excepturi velit sequi. Unde officiis enim deleniti veniam aperiam sequi nostrum repellendus, placeat, aspernatur rerum necessitatibus nisi iure ratione voluptates tempora ut id laborum aut delectus tempore quae harum voluptatibus! Voluptatibus dignissimos expedita cupiditate quas saepe sit ipsam deserunt laboriosam fuga nemo voluptatem consequuntur, temporibus ut maxime suscipit illo modi porro quisquam aliquam distinctio nihil itaque at. Est quasi optio quam harum rerum quaerat a error facere, eaque debitis itaque cumque fuga tempora distinctio consequatur esse quidem, numquam non repudiandae odio quae consectetur ut architecto.</p>
+    </div>
+     
+    </>
+  )
+}
+
+export default App
 
