f969da0914451cfdea37523be365965b3a7ea365
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-26 14:15:33 +0300

    Create part-z-0014-ternary1.rs

---
+++ part-z-0014-ternary1.rs
@@ -0,0 +1,14 @@
+fn main() {
+
+    let age: u8 = 17;
+
+    let result: &str = if age > 18 {"Adult"} else {"Child"};
+
+    println!("Is he/she a child/adult: {result}"); // Is he/she a child/adult: Child
+
+}
+
+
+
+
+
