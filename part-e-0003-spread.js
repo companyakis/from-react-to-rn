fdc9a28f863f22e7f6427ba0eadc289b9417981d
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-28 09:09:50 +0300

    Create part-e-0003-spread.js

---
+++ part-e-0003-spread.js
@@ -0,0 +1,13 @@
+let years = [2018, 2020, 2021, 2022]
+
+let years2 = [...years, 2023, 2024]
+
+console.log(years2)
+
+let years3 = [2007, ...years]
+
+console.log(years3)
+
+// [ 2018, 2020, 2021, 2022, 2023, 2024 ]
+
+// [ 2007, 2018, 2020, 2021, 2022 ]
