8672a81b90d75a91e402ffd648f7ee450147344f
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-31 17:01:22 +0300

    Create part-a-0098-simple-flex-products-design2-App.jsx

---
+++ part-a-0098-simple-flex-products-design2-App.jsx
@@ -0,0 +1,20 @@
+import "./App.css"
+import AppHeader from "./components/AppHeader"
+import Products from "./components/Products"
+
+
+function App() {
+  
+
+  return (
+    <div>
+
+      <AppHeader />
+      <Products />
+
+    </div>
+     
+  )
+}
+
+export default App
