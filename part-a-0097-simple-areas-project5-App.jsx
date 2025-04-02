cba155445095328511fb5096cd57562c141229aa
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-04-02 11:02:37 +0300

    Create part-a-0097-simple-areas-project5-App.jsx

---
+++ part-a-0097-simple-areas-project5-App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Areas from "./components/Areas";
+import Header from "./components/Header";
+
+Areas;
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <Areas />
+    </>
+  );
+}
+
+export default App;
