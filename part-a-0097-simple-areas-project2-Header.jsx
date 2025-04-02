e315f88c03e09da4afeb975aa9971c9119f6747b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-04-02 09:42:42 +0300

    Create part-a-0097-simple-areas-project2-Header.jsx

---
+++ part-a-0097-simple-areas-project2-Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='header-area'>
+      Build Your Future & Important Areas
+    </div>
+  )
+}
+
+export default Header
