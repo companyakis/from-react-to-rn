bd714a84f8a168b0005ea67f8b45e7a18bfdae42
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-04-07 10:24:42 +0300

    Update part-b-0006-router-dom5-Header.jsx

--- part-b-0006-router-dom5-Header.jsx
+++ part-b-0006-router-dom5-Header.jsx
@@ -6,10 +6,15 @@ function Header() {
   return (
     <div className='header'>
 
-        <Link className="link" to="/" >Home</Link>
-        <Link className="link" to="/products" >Products</Link>
-        <Link className="link" to="/about" >About</Link>
-        <Link className="link" to="/contact" >Contact</Link>
+        <div>
+            <Link className="link" to="/" >Home</Link>
+            <Link className="link" to="/products" >Products</Link>
+            <Link className="link" to="/about" >About</Link>
+        </div>
+        
+        <div>
+            <Link className="link" to="/contact" >Contact</Link>
+        </div>
       
     </div>
   )
